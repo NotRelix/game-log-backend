99b515d6e71d5ef6087e3214977c0fa848904e33
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-08-10 20:57:23 +0800

    chore: setup drizzle as ORM

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/node-postgres";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const db = drizzle(process.env.DATABASE_URL!);
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  }
+);
