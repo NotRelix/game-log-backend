080efbd64d081cb0706cfedb6cb80ed273dab38d
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-08-12 13:57:15 +0800

    feat: add mock test for createPost

--- src/controllers/posts.ts
+++ src/controllers/posts.ts
@@ -1,14 +1,22 @@
 import { createFactory } from "hono/factory";
+import type { InsertPost } from "../db/types.ts";
+import { insertPost } from "../db/query.ts";
 
 const factory = createFactory();
 
-export const getPosts = factory.createHandlers(
-  async (c) => {
-    return c.json("getting posts");
+export const getPosts = factory.createHandlers(async (c) => {
+  return c.json("getting posts");
 });
 
-export const createPost = factory.createHandlers(
-  async (c) => {
-    return c.json("creating post");
-  }
-)
+export const createPost = factory.createHandlers(async (c) => {
+  // TODO: add validations
+  // Mock test
+  const body = await c.req.json();
+  const newPost: InsertPost = {
+    title: body.title,
+    body: body.body,
+    userId: 1,
+  };
+  const post = await insertPost(newPost);
+  return c.json(post);
+});
--- src/db/query.ts
+++ src/db/query.ts
@@ -1,7 +1,12 @@
 import { eq } from "drizzle-orm";
 import { db } from "./drizzle.ts";
-import { usersTable } from "./schema.ts";
-import type { InsertUser, SelectUser } from "./types.ts";
+import { postsTable, usersTable } from "./schema.ts";
+import type {
+  InsertPost,
+  InsertUser,
+  SelectPost,
+  SelectUser,
+} from "./types.ts";
 
 export const getUser = async (username: string): Promise<SelectUser | null> => {
   const user = await db
@@ -16,4 +21,11 @@ export const insertUser = async (
 ): Promise<SelectUser | null> => {
   const user = await db.insert(usersTable).values(newUser).returning();
   return user[0] ?? null;
+};
+
+export const insertPost = async (
+  newPost: InsertPost
+): Promise<SelectPost | null> => {
+  const post = await db.insert(postsTable).values(newPost).returning();
+  return post[0] ?? null;
 };
