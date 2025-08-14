a2d61d541514bd0111ca18fab8fa0e97e952c521
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-08-14 22:51:32 +0800

    refactor: format zod errors

--- src/controllers/register.ts
+++ src/controllers/register.ts
@@ -4,17 +4,29 @@ import { userSchema } from "../validators/user.ts";
 import { getUserDb, insertUserDb } from "../db/query.ts";
 import type { InsertUser } from "../db/types.ts";
 import bcrypt from "bcrypt";
+import type { ZodIssue } from "zod/v3";
 
 const factory = createFactory();
 
 export const registerUserHandler = factory.createHandlers(
-  zValidator("json", userSchema),
+  zValidator("json", userSchema, (result, c) => {
+    if (!result.success) {
+      const error = JSON.parse(result.error.message);
+      return c.json(
+        {
+          success: false,
+          messages: error.map((err: ZodIssue) => err.message),
+        },
+        400
+      );
+    }
+  }),
   async (c) => {
     try {
       const body = c.req.valid("json");
       const user = await getUserDb(body.username);
       if (user) {
-        return c.json({ error: "User already exists" }, 400);
+        return c.json({ message: "User already exists" }, 400);
       }
       const hashedPassword = await bcrypt.hash(body.password, 10);
       const newUser: InsertUser = {
@@ -23,12 +35,15 @@ export const registerUserHandler = factory.createHandlers(
       };
       const newUserResult = await insertUserDb(newUser);
       if (!newUserResult) {
-        return c.json({ error: "Something went wrong creating the user" }, 500);
+        return c.json(
+          { message: "Something went wrong creating the user" },
+          500
+        );
       }
       const { password, ...safeUser } = newUserResult;
       return c.json(safeUser, 201);
     } catch (err) {
-      return c.json({ error: "Failed to register user" }, 500);
+      return c.json({ message: "Failed to register user" }, 500);
     }
   }
 );
--- src/validators/user.ts
+++ src/validators/user.ts
@@ -1,6 +1,10 @@
 import { z } from "zod";
 
 export const userSchema = z.object({
-  username: z.string().min(3).max(16).toLowerCase(),
-  password: z.string().min(8),
+  username: z
+    .string()
+    .min(3, "Username must be at least 3 characters")
+    .max(16, "Username must be under 16 characters")
+    .toLowerCase(),
+  password: z.string().min(8, "Password must be at least 8 characters"),
 });
