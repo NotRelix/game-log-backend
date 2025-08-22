b10836a01bf385073b732a6efa838d648984dc2d
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-08-22 15:43:19 +0800

    feat: add edit post functionality

--- src/controllers/posts.ts
+++ src/controllers/posts.ts
@@ -111,43 +111,71 @@ export const getPostHandler = factory.createHandlers(async (c) => {
   }
 });
 
-export const editPostHandler = factory.createHandlers(
-  validator(postSchema.partial()),
-  async (c) => {
-    try {
-      const postId = Number(c.req.param("postId"));
-      if (isNaN(postId)) {
-        return c.json({ success: false, messages: ["Invalid post id"] }, 400);
-      }
-      const post = await getPostDb(postId);
-      if (!post) {
-        return c.json(
-          { success: false, messages: ["Post doesn't exist"] },
-          404
-        );
-      }
-      const user = c.get("jwtPayload");
-      if (post.authorId !== user.id) {
-        return c.json(
-          { success: false, messages: ["Unauthorized access"] },
-          403
-        );
-      }
-      const newPost = await c.req.valid("json");
-      const updated = await editPostDb(postId, newPost);
-      return c.json(
-        {
-          success: true,
-          messages: ["Successfully edited post"],
-          post: updated,
-        },
-        200
-      );
-    } catch (err) {
-      return c.json({ success: false, messages: ["Failed to edit post"] }, 500);
+export const editPostHandler = factory.createHandlers(async (c) => {
+  try {
+    const postId = Number(c.req.param("postId"));
+    if (isNaN(postId)) {
+      return c.json({ success: false, messages: ["Invalid post id"] }, 400);
     }
+    const post = await getPostDb(postId);
+    if (!post) {
+      return c.json({ success: false, messages: ["Post doesn't exist"] }, 404);
+    }
+    const user = c.get("jwtPayload");
+    if (post.authorId !== user.id) {
+      return c.json({ success: false, messages: ["Unauthorized access"] }, 403);
+    }
+    const body = await c.req.parseBody();
+    const parsed = postSchema.safeParse({
+      title: body.title,
+      body: body.body,
+      published: body.published === "true",
+    });
+    if (!parsed.success) {
+      return c.json({ success: false, messages: parsed.error }, 400);
+    }
+
+    const file = body.headerImg as File;
+    let filePath = null;
+    if (file) {
+      const arrayBuffer = await file.arrayBuffer();
+      const safeOriginalName = file.name
+        .normalize("NFKD")
+        .replace(/[^\x00-\x7F]/g, "")
+        .replace(/\s+/g, "_");
+
+      filePath = `${Date.now()}-${safeOriginalName}`;
+      const { data, error } = await supabase.storage
+        .from("uploads")
+        .upload(filePath, arrayBuffer, {
+          contentType: file.type,
+        });
+      if (error) throw error;
+    }
+
+    const headerImgPath = filePath
+      ? `${process.env.SUPABASE_URL}/storage/v1/object/public/uploads/${filePath}`
+      : null;
+    const newPost: InsertPost = {
+      title: parsed.data.title,
+      body: parsed.data.body,
+      authorId: user.id,
+      published: body.published === "true",
+      ...(filePath && { headerImgPath: headerImgPath }),
+    };
+    const updated = await editPostDb(postId, newPost);
+    return c.json(
+      {
+        success: true,
+        messages: ["Successfully edited post"],
+        post: updated,
+      },
+      200
+    );
+  } catch (err) {
+    return c.json({ success: false, messages: ["Failed to edit post"] }, 500);
   }
-);
+});
 
 export const deletePostHandler = factory.createHandlers(async (c) => {
   try {
